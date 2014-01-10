4439dbcbc95e95c76561b22d755bba737614dc5b
Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
2014-01-10 03:12:08 -0200

    CameraService.cpp: disable shutter sound
    As in Ubuntu Touch we don't have support for audioflinger anymore.
    
    Change-Id: I6115bc5c1e9606a7af6b19716d0c25036128e011
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -881,8 +881,8 @@ void CameraService::loadSound() {
     LOG1("CameraService::loadSound ref=%d", mSoundRef);
     if (mSoundRef++) return;
 
-    mSoundPlayer[SOUND_SHUTTER] = newMediaPlayer("/system/media/audio/ui/camera_click.ogg");
-    mSoundPlayer[SOUND_RECORDING] = newMediaPlayer("/system/media/audio/ui/VideoRecord.ogg");
+    mSoundPlayer[SOUND_SHUTTER] = NULL;
+    mSoundPlayer[SOUND_RECORDING] = NULL;
 }
 
 void CameraService::releaseSound() {
